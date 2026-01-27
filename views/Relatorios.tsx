630b45cda78e1c745c024572e49237361b986e6d
007-sistemas <websystemas007@gmail.com>
2026-01-27 16:32:05 -0300

    fix(relatorios): fechamento de tags e correção de erro de build (JSX)

--- views/Relatorios.tsx
+++ views/Relatorios.tsx
@@ -948,5 +948,16 @@ export const Relatorios: React.FC = () => {
                             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                           />
                         </div>
+
                       </div>
                     </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
